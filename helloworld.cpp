cc051df5154c46c2ff990100b39c0fca5985981d
Macbook <macbook@Macbooks-MacBook-Air.local>
2023-09-28 00:22:36 +0700

    coba cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,5 +1,34 @@
 #include <stdio.h>
-
-int main(){
-   printf("Coba cpp");
+int main()
+{
+    int jb,mb,db,js,ms,ds, total, detiksa, detikbe, ja, ma, da;
+    printf("jam berangkat = "); scanf("%d",&jb);
+    printf("menit berangkat = "); scanf("%d",&mb);
+    printf("detik berangkat = "); scanf("%d",&db);
+    printf("jam sampai = "); scanf("%d",&js);
+    printf("menit sampai = "); scanf("%d",&ms);
+    printf("detik sampai = "); scanf("%d",&ds); 
+    
+    jb = jb*3600;
+    mb = mb*60;
+    db = db;
+    detiksa = jb+mb+db;
+    
+    js = js*3600;
+    ms = ms*60;
+    ds = ds;
+    detikbe = js+ms+ds;
+    
+    total = detikbe - detiksa;
+    
+    ja = total/3600;
+    total = total%3600;
+    ma = total/60;
+    da = total%60;
+    
+    printf(" Lama perjalanan = ");
+    printf("%d jam ",ja);
+    printf("%d menit ",ma);
+    printf("%d detik",da);
+    
 }
